f9272f23e0e2c2027e9b773024b517f0beb2371a
AnitaMjaow <olsson_anita@live.se>
2024-05-05 15:47:24 +0200

    Test2 + Fel + Resolved + kör npm test

--- src/stack.js
+++ src/stack.js
@@ -7,3 +7,18 @@ exports.push = function (x) {
 	stack.push(x);
 };
 
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+	return stack.pop();
+}
+
+// Returnerar det översta elementet i stacken
+exports.peek = function () {
+	return _.last(stack);
+}
+
+
+// remove or comment out functions push, pop, peek of choice to test missingFunctions in file: ../tests/stack.test.js
+
+
+
