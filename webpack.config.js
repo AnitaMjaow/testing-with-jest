127df97d2b4886a851078dd4c4494a5c40c36937
AnitaMjaow <olsson_anita@live.se>
2024-05-05 16:05:13 +0200

    Added: npm install copy-webpack-plugin --save-dev in package.json, package-lock.json. Further changes in webpack.config.js from development to production, added CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
+const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-	mode: 'development', // or 'production' or 'none'
+	mode: 'production', // or 'production' or 'none'
 	entry: './src/index.js', // your entry file
 	output: {
-		filename: 'bundle.js', // output filename
-		path: __dirname + '/dist' // output directory
-	}
+		path: path.resolve(__dirname, 'dist'), // output filename
+		filename: 'main.js'// output directory
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [{
+				context: __dirname + '/src/',
+				from: '*.html'
+			}]
+		})
+	]
 };
+
+// ERROR in unable to locate 'C:/Users/olsso/projekt/git-test/testing-with-jest/src/*.html' glob
+// webpack 5.82.1 compiled with 1 error in 708 ms
 
 //Set 'mode' option to 'development' or 'production' to enable defaults for each environment.
 //You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode/
